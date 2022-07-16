74cbdff07c2e2de7c289bb8cde3865bdf6e3f038
ibrahimibrahim <ibrahimib1201@gmail.com>
2022-07-16 10:48:05 -0400

    trigger 3

--- src/test/java/cydeo/CukesRunner.java
+++ src/test/java/cydeo/CukesRunner.java
@@ -10,6 +10,7 @@ import org.junit.runner.RunWith;
                 "json:target/cucumber.json",
                 "html:target/cucumber/report.html",
                 "junit:target/junit/junit-report.xml",
+
                 "rerun:target/rerun.txt",},
         features = "src/test/resources/features",
 
